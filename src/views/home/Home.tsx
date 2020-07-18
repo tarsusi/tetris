610b55a727c0a1a2213725303c96686ebbddfd6a
Ömer Burak KARATAŞ <omer.burak.karatas@outlook.com>
2020-07-18 18:51:22 +0300

    feat: add nes.css and apply on home screen

--- src/views/home/Home.tsx
+++ src/views/home/Home.tsx
@@ -5,12 +5,25 @@ import './home.scss';
 const Home: React.FC = () => {
   return (
     <div className="home-container">
-      <Link className="app-header-link" to="/play">
-        Play
-      </Link>
-      <Link className="app-header-link" to="/settings">
-        Settings
-      </Link>
+      <div className="nes-container with-title is-centered">
+        <p className="title nes-text is-primary">Welcome Tetris Game!</p>
+        <div className="nes-container-body">
+          <Link className="app-header-link" to="/play">
+            Play
+          </Link>
+          <Link className="app-header-link" to="/settings">
+            Settings
+          </Link>
+        </div>
+      </div>
+      <div
+        className="github-link nes-pointer"
+        onClick={() =>
+          window.open('https://github.com/tarsusi/tetris', '_blank')
+        }
+      >
+        <i className="nes-octocat animate"></i>
+      </div>
     </div>
   );
 };
